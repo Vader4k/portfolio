d3858d0d25369572412037e779a0a54c371c0b53
danie odukwe <kingdanny295@gmail.com>
2024-04-11 15:19:09 +0100

    @add/ done routing initial pages

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from './Home'
+import Works from './Works'
+import About from './About'
+import Articles from './Articles'
+import Contact from './Contact'
+import Error from './ErrorPage'
+
+export {
+    Home,
+    Works,
+    About,
+    Articles,
+    Contact,
+    Error
+}
---
+++ src/routes/Routing.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom"
+import { Home, About, Works, Contact, Articles, Error } from '../pages'
+const Routing = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/Articles" element={<Articles />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  )
+}
+
+export default Routing
