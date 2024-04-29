6c0fc3c96a4161693e372e47b3443fd4d7ee7b20
danie odukwe <kingdanny295@gmail.com>
2024-04-29 12:20:39 +0100

    @fix/ make animation different for pages that i want

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,15 +1,24 @@
 import Routing from '../routes/Routing'
 import Navbar from '../components/Navbar'
 import { motion } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
 
 const Layout = () => {
+  const location = useLocation();
+
+  const containerVariants = {
+    home: { y: 0, transition: {duration: 0.8, ease: "easeOut"}},
+    other: { y: 0, transition: {duration: 0.8, ease: "easeOut"}},
+    exit: { y: location.pathname === '/' || location.pathname.startsWith('/project/') ? "100vh" : "-100vh", transition: {duration: 0.8, ease: "easeOut"}}
+  }
+
   return (
-    <section className='md:flex'>
+    <section className='md:flex w-full'>
       <motion.div
-       className='h-screen w-screen fixed bg-text-50 z-40'
-       animate={{y: '100vh'}}
-       exit={{y: '100vh'}}
-       transition={{duration: 1, ease: "easeInOut"}} 
+       className={`h-screen w-screen fixed bg-text-50 z-40`}
+       variants={containerVariants}
+       initial={location.pathname === '/' || location.pathname.startsWith('/project/')  ? "home" : "other"}
+       animate="exit" 
       />
         <Navbar />
         <main className='w-full'>
