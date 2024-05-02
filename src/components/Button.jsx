912843a2a69249270349cfc0cea80fea8d7971a5
danie odukwe <kingdanny295@gmail.com>
2024-05-02 15:41:05 +0100

    @fix/ made a better button animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,11 @@ const Button = ({ text, path, target }) => {
   return (
     <Link to={path} target={target}>
       <motion.div
-        initial={{ y: "-50vh" }}
+        initial={{ y: "-20vh" }}
         whileInView={{ y: 0 }}
         viewport={{ once: false }} 
-        transition={{ duration: 0.8, ease: "easeOut" }}
-        className="max-h-[60px] overflow-hidden">
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        className="max-h-[80px] overflow-hidden">
         <button className=" px-8 py-3 my-6 bg-text-50 text-bg-50 hover:px-16 rounded-[50px] transition-all lg:text-[1.2rem] h1">
               {text}
           </button>
