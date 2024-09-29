7073bb36086c4d9345d53afe5e48167fe238d07b
danie odukwe <kingdanny295@gmail.com>
2024-09-29 08:26:51 +0100

    @fix / Refactor Layout component for improved readability and performance
    - Simplified route condition checks with isHomeOrProject variable
    - Removed unnecessary commas in containerVariants object
    - Cleaned up className prop syntax in motion.div
    - Adjusted indentation for better code readability

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -7,24 +7,28 @@ const Layout = () => {
   const location = useLocation();
 
   const containerVariants = {
-    home: { y: 0, transition: {duration: 0.8, ease: "easeOut",}},
-    other: { y: 0, transition: {duration: 0.8, ease: "easeOut"}},
-    exit: { y: location.pathname === '/' || location.pathname.startsWith('/project/') ? "120vh" : "-120vh", transition: {duration: 0.8, ease: "easeOut"}}
+    home: { y: 0, transition: { duration: 0.8, ease: "easeOut" }},
+    other: { y: 0, transition: { duration: 0.8, ease: "easeOut" }},
+    exit: { 
+      y: location.pathname === '/' || location.pathname.startsWith('/project/') ? "120vh" : "-120vh", 
+      transition: { duration: 0.8, ease: "easeOut" }
+    }
   }
 
+  const isHomeOrProject = location.pathname === '/' || location.pathname.startsWith('/project/');
+
   return (
-    <section className='md:flex w-full'>
+    <section className='w-full md:flex'>
       <motion.div
-       className={`h-screen w-screen fixed bg-text-50 z-40`}
-       variants={containerVariants}
-       initial={location.pathname === '/' || location.pathname.startsWith('/project/')  ? "home" : "other"}
-       animate="exit" 
+        className='fixed z-40 w-screen h-screen bg-text-50'
+        variants={containerVariants}
+        initial={isHomeOrProject ? "home" : "other"}
+        animate="exit" 
       />
-        <Navbar />
-        <main 
-          className='w-full'>
-          <Routing />
-        </main>
+      <Navbar />
+      <main className='w-full'>
+        <Routing />
+      </main>
     </section>
   )
 }
